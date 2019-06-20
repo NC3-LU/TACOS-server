3d8884adfe0fcd094eddcf2afca4774f8cadaaef
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-06-20 10:27:22 +0200

    chg: [API] Handles HTTP GET/id requests.

--- src/web/views/api/v1/__init__.py
+++ src/web/views/api/v1/__init__.py
@@ -6,6 +6,7 @@ from bootstrap import application
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
-from web.views.api.v1.spam import SpamListAPI
+from web.views.api.v1.spam import SpamListAPI, SpamAPI
 
 api.add_resource(SpamListAPI, '/api/v1/spams', endpoint='spams')
+api.add_resource(SpamAPI, '/api/v1/spams/<int:id>', endpoint = 'spam')
--- src/web/views/api/v1/spam.py
+++ src/web/views/api/v1/spam.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask_restful import Resource, reqparse, fields, marshal
+from flask import jsonify
+from flask_restful import (Resource, reqparse, fields, marshal, abort)
 
 from web.models import Spam
 
@@ -12,9 +13,14 @@ spam_fields = {
     'created': fields.DateTime
 }
 
+def make_public_spam(spam):
+    """Convert the internal representation of a spam to
+    the external representation that clients expected."""
+    return marshal(spam, spam_fields)
+
+
 class SpamListAPI(Resource):
     #decorators = [auth.login_required]
-
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('type', type=str, required=True,
@@ -31,3 +37,11 @@ class SpamListAPI(Resource):
             'objects': [marshal(spam, spam_fields)
                             for spam in spams.all()]
         }
+
+
+class SpamAPI(Resource):
+    def get(self, id):
+        spam = Spam.query.filter(Spam.id==id).first()
+        if not spam:
+            return abort(404, message='Spam {} does not exist.'.format(id))
+        return jsonify({'spam': make_public_spam(spam)})
