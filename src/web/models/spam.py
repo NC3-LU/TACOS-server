f5f35be274ed9216ab8d1943bfe9023944634b57
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-06-25 08:20:02 +0200

    chg: [model] added UUID field for spam object

--- src/web/models/spam.py
+++ src/web/models/spam.py
@@ -1,5 +1,6 @@
 
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
 
 from bootstrap import db
 
@@ -7,6 +8,7 @@ class Spam(db.Model):
     """Represent a spam report.
     """
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), unique=True, nullable=False)
     number = db.Column(db.Text(), nullable=False)
     category = db.Column(db.Text(), nullable=False)
     source =  db.Column(db.Text(), nullable=False)
