a4e84c5c15d52a3d7155b87bfe74286ba0fab9d5
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-07-11 08:44:19 +0200

    chg: [model] Generate a default value for UUIDs

--- src/web/models/spam.py
+++ src/web/models/spam.py
@@ -1,5 +1,6 @@
 
 from datetime import datetime
+from uuid import uuid4
 from sqlalchemy.dialects.postgresql import UUID
 
 from bootstrap import db
@@ -9,9 +10,15 @@ class Spam(db.Model):
     """Represent a spam report.
     """
     id = db.Column(db.Integer, primary_key=True)
-    uuid = db.Column(UUID(as_uuid=True), unique=True, nullable=False)
+    uuid = db.Column(UUID(as_uuid=True), unique=True, nullable=False,
+                    default=uuid4)
     number = db.Column(db.Text(), nullable=True)
     number_hash = db.Column(db.Text(), nullable=True)
     category = db.Column(db.Text(), nullable=False)
     source =  db.Column(db.Text(), nullable=False)
     date = db.Column(db.DateTime(), default=datetime.utcnow())
+
+    def __str__(self):
+        return """UUID: {}
+Number: {}
+Date: {}""".format(self.uuid, self.number, self.date)
